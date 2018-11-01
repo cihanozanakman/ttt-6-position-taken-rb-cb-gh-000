011ea960e44de3237be7a0d1e742310fd942c3f6
Cihan Akman <cihanozanakman@gmail.com>
2018-11-01 20:24:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,4 +3,7 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     return false
   end
+  if board[index] == "X" || board[index] == "O"
+    return false
+  end
 end
